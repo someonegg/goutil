3a49798413ad0584a3462ca2ce16803522a21e31
someonegg <someonegg@163.com>
2016-12-12 11:18:40 +0800

    initial commit

---
+++ idgener/sql.go
@@ -0,0 +1,59 @@
+// Copyright 2015 someonegg. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package idgener
+
+import (
+	"database/sql"
+	"github.com/someonegg/gox/dbx"
+	"golang.org/x/net/context"
+)
+
+type sqlGener struct {
+	db *dbx.SQLDB
+	tn string
+}
+
+func NewSqlGener(driver, dsn, tn string,
+	maxConcurrent int) (IDGener, error) {
+
+	_db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = _db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	_, err = _db.Exec(`CREATE TABLE IF NOT EXISTS ? (
+		id BIGINT AUTO_INCREMENT NOT NULL,
+		PRIMARY KEY (id)
+		)`, tn)
+	if err != nil {
+		return nil, err
+	}
+	_db.Exec("INSERT INTO ? (id) VALUES (0)", tn)
+
+	db := dbx.NewSQLDB(_db, maxConcurrent)
+	return &sqlGener{db: db, tn: tn}, nil
+}
+
+func (g *sqlGener) Close() error {
+	return g.db.Close()
+}
+
+func (g *sqlGener) GenID(ctx context.Context) (int64, error) {
+	res, err := g.db.Exec(ctx, "INSERT INTO ? () VALUES ()", g.tn)
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	if id > 0 {
+		g.db.Exec(ctx, "DELETE FROM ? WHERE id = ?", g.tn, id-1)
+	}
+	return id, nil
+}
